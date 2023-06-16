44d3b4d6f2f88318b9f08564178ea450183e250b
OscarCarde <oscrdns@gmail.com>
2023-06-16 20:10:00 +0100

    completed send procedure

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -10,9 +10,12 @@ document.addEventListener('DOMContentLoaded', function() {
   load_mailbox('inbox');
 
   document.querySelector("#compose-form").addEventListener("submit", event => {
-    send();
-    load_mailbox('sent');
     event.preventDefault();
+    send()
+      .then(() => {
+        load_mailbox('sent');
+      })
+    
   });
 });
 
@@ -29,7 +32,7 @@ function compose_email() {
 }
 
 function load_mailbox(mailbox) {
-  
+
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
@@ -37,24 +40,34 @@ function load_mailbox(mailbox) {
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
-  if(mailbox === 'inbox') {
-    let emails = getInboxMails();
-    console.log(emails);
-    emails.forEach(email => {
-      let email_p = document.createElement("p");
-      email_p.innerHTML = email["id"];
-      document.querySelector("#email-view").appendChild(email_p);
-      
-    });
+  if (mailbox === 'inbox') {
+    getInboxMails()
+      .then(emails => {
+        if (emails.length > 0) {
+          emails.forEach(email => {
+            let email_p = document.createElement("p");
+            email_p.innerHTML = email["id"];
+            document.querySelector("#emails-view").appendChild(email_p);
+          });
+        } else {
+          let noEmails = document.createElement('h3');
+          noEmails.innerHTML = "No emails";
+          document.querySelector("#emails-view").appendChild(noEmails);
+        }
+      })
+      .catch(error => {
+        console.log("Something went wrong while trying to retreive the emails fom the API", error);
+      });
   }
 }
+
 
 function send() {
   let recipients = document.querySelector('#compose-recipients').value;
   let subject = document.querySelector('#compose-subject').value;
   let body = document.querySelector('#compose-body').value;
 
-  fetch("/emails", {
+  return fetch("/emails", {
     method:'POST',
     body: JSON.stringify({
       recipients: recipients,
@@ -69,7 +82,7 @@ function send() {
 }
 
 function getInboxMails() {
-  fetch("emails/inbox")
-  .then(response => response.json())
-  .then(emails =>  emails);
+  return fetch("emails/inbox")
+    .then(response => response.json())
+    .then(emails =>  emails);
 }
